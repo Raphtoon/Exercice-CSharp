2ad3fc024e36f766c02320353427e1c32de4138f
Raphaël Oliazadeh <Oliazadeh.raphael@gmail.com>
2024-01-25 14:36:40 +0100

    initialize commit

---
+++ Exercice3.Bibliotheque/Exercice3.Bibliotheque/SearchCity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice3.Bibliotheque
+{
+
+    public class SearchCity
+    {
+        private List<string> _cities = new List<string>();
+        public List<string> SearchCityByName(string word);
+    }
+}
---
+++ Exercice3.Bibliotheque/Exercice3.NUnit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Exercice3.NUnit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
